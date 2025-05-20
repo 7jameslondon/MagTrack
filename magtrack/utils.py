85143d6cac595f18b29e3b87339df0d036bf076b
James London <7jameslondon@gmail.com>
2025-05-20 09:48:22 -0400

    Added an airy disk generator

--- magtrack/utils.py
+++ magtrack/utils.py
@@ -1,5 +1,6 @@
 import cupy as cp
 import numpy as np
+import scipy as sp
 
 
 def split_gpu_apply(stack, n, func, *args, **kwargs):
@@ -8,7 +9,7 @@ def split_gpu_apply(stack, n, func, *args, **kwargs):
     n_mod = n_images % n
 
     # First split
-    gpu_substack = cp.asarray(stack[:, :, 0:n]).astype('float32')
+    gpu_substack = cp.asarray(stack[:, :, 0:n]).astype('float64')
     gpu_args = []
     for arg in args:
         gpu_args.append(cp.asarray(arg[0:n]))
@@ -19,23 +20,38 @@ def split_gpu_apply(stack, n, func, *args, **kwargs):
     # Middle splits
     for s in range(1, n_splits):
         gpu_substack = cp.asarray(stack[:, :, (s * n):((s + 1) * n)]
-                                  ).astype('float32')
+                                  ).astype('float64')
         gpu_args = []
         for arg in args:
             gpu_args.append(cp.asarray(arg[(s * n):((s + 1) * n)]))
         sub_results = func(gpu_substack, *gpu_args, **kwargs)
         for i in range(len(results)):
-            results[i] = np.append(results[i], cp.asnumpy(sub_results[i]))
+            results[i] = np.concatenate((results[i], cp.asnumpy(sub_results[i])), axis=-1)
 
     # Last split
     if n_mod > 0:
-        gpu_substack = cp.asarray(stack[:, :,
-                                        (n_splits * n):]).astype('float32')
+        gpu_substack = cp.asarray(stack[:, :, (n_splits * n):]).astype('float64')
         gpu_args = []
         for arg in args:
             gpu_args.append(cp.asarray(arg[(n_splits * n):]))
         sub_results = func(gpu_substack, *gpu_args, **kwargs)
         for i in range(len(results)):
-            results[i] = np.append(results[i], cp.asnumpy(sub_results[i]))
+            results[i] = np.concatenate((results[i], cp.asnumpy(sub_results[i])), axis=-1)
 
     return results
+
+def airy_disk(size=512, radius=50, wavelength=1.0):
+    """ Generate an Airy disk pattern """
+    x = np.linspace(-size / 2, size / 2, size)
+    y = np.linspace(-size / 2, size / 2, size)
+    xx, yy = np.meshgrid(x, y)
+    r = np.sqrt(xx ** 2 + yy ** 2)
+    r = np.where(r == 0, 1e-10, r)  # Avoid division by zero
+
+    k = 2 * np.pi / wavelength
+    kr = k * r / radius
+    intensity = (2 * sp.special.j1(kr) / kr) ** 0.8
+
+    intensity[r >= radius*4] = 0
+
+    return intensity
