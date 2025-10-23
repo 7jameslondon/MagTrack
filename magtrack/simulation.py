c0c80bf0bb7fb46d224771d984e0ad396ecc21a9
James London <7jameslondon@gmail.com>
2025-10-23 11:18:28 -0400

    Fixed bug in airy_disk for cupy based scipy import

--- magtrack/simulation.py
+++ magtrack/simulation.py
@@ -7,7 +7,7 @@ import scipy as sp
 
 def airy_disk(xp=np, size=64, radius=10, wavelength=1.0):
     """ Generate an Airy disk pattern """
-    xpx = sp if xp is np else cupyx
+    xpx = sp if xp is np else cupyx.scipy
     x = xp.linspace(-size / 2, size / 2, size)
     y = xp.linspace(-size / 2, size / 2, size)
     xx, yy = xp.meshgrid(x, y)
@@ -16,12 +16,16 @@ def airy_disk(xp=np, size=64, radius=10, wavelength=1.0):
 
     k = 2 * xp.pi / wavelength
     kr = k * r / radius
-    intensity = (2 * xpx.special.j1(kr) / kr)**0.8
+    intensity = 2 * xpx.special.j1(kr) / kr
 
     intensity[r >= radius * 4] = 0
 
     return intensity
 
 if __name__ == '__main__':
-    image = airy_disk(xp=cp)
-    print(image)
+    import matplotlib.pyplot as plt
+    image = airy_disk(xp=np)
+    plt.imshow(image)
+    plt.show(block=False)
+
+
