dfa5e8d420b7fe4f897916d3d864972bbffe71c0
James London <7jameslondon@gmail.com>
2025-11-02 17:12:01 -0500

    Updated doc build config for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 project = 'MagTrack'
 copyright = '2025, James London'
@@ -15,16 +10,23 @@ release = '0.7.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.intersphinx",
+    "sphinx_design",
+    'sphinx.ext.inheritance_diagram',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+autoapi_type = "python"
+autoapi_dirs = ["../../magtrack"]
+autosummary_generate = True
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
