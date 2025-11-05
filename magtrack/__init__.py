5922064e7ae67cf82b42d69a70b5295328691a51
James London <7jameslondon@gmail.com>
2025-11-05 11:30:29 -0500

    Removed redundant `__all__` definition from `__init__.py`.

--- magtrack/__init__.py
+++ magtrack/__init__.py
@@ -4,6 +4,3 @@ filterwarnings('ignore', category=FutureWarning, message='cupyx.jit.rawkernel')
 
 from magtrack.core import *  # noqa: F401,F403
 from magtrack._cupy import check_cupy
-
-__all__ = list(globals().get('__all__', []))
-__all__.append('check_cupy')
