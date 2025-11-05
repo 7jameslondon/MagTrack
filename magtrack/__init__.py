1aae2c3cefb1ae30544dd39ad0b7380b9a47f6ed
James London <7jameslondon@gmail.com>
2025-11-05 11:17:08 -0500

    Corrected relative import in `core.py` to use an absolute path for `magtrack._cupy`.

--- magtrack/__init__.py
+++ magtrack/__init__.py
@@ -3,7 +3,7 @@ from warnings import filterwarnings
 filterwarnings('ignore', category=FutureWarning, message='cupyx.jit.rawkernel')
 
 from magtrack.core import *  # noqa: F401,F403
-from ._cupy import check_cupy
+from magtrack._cupy import check_cupy
 
 __all__ = list(globals().get('__all__', []))
 __all__.append('check_cupy')
