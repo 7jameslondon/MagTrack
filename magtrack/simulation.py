04a0a8608fc0ffeb14a1877272348481ab0016a8
Jamie (James) London <7jameslondon@gmail.com>
2025-11-21 13:10:40 -0500

    Removed unused airy-disk function in simulation module

--- magtrack/simulation.py
+++ magtrack/simulation.py
@@ -4,23 +4,6 @@ import numpy as np
 import scipy as sp
 from magtrack._cupy import cp, cupyx
 
-def airy_disk(xp=np, size=64, radius=10, wavelength=1.0):
-    """ Generate an Airy disk pattern """
-    xpx = sp if xp is np else cupyx.scipy
-    x = xp.linspace(-size / 2, size / 2, size)
-    y = xp.linspace(-size / 2, size / 2, size)
-    xx, yy = xp.meshgrid(x, y)
-    r = xp.sqrt(xx**2 + yy**2)
-    r = xp.where(r == 0, 1e-10, r)  # Avoid division by zero
-
-    k = 2 * xp.pi / wavelength
-    kr = k * r / radius
-    intensity = 2 * xpx.special.j1(kr) / kr
-
-    intensity[r >= radius * 4] = 0
-
-    return intensity
-
 def simulate_beads(
     xyz_nm, # array of [x_nm, y_nm, z_nm]
     nm_per_px: float = 100.0,
